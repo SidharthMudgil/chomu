7d96ee07a74ea49d0c41e27d29e63214513d1251
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-11-11 21:51:09 +0530

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+buildscript {
+    val agp_version by extra("8.1.3")
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
