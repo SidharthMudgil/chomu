1e3bcd2c2ee954728ad029b0b22f95111fbbd3d3
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-11-12 15:45:55 +0530

    add InterviewFragment

---
+++ app/src/main/java/com/sidharth/chomu/InterviewFragment.kt
@@ -0,0 +1,22 @@
+package com.sidharth.chomu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sidharth.chomu.databinding.FragmentInterviewBinding
+
+class InterviewFragment : Fragment() {
+
+    private lateinit var fragmentInterviewBinding: FragmentInterviewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentInterviewBinding = FragmentInterviewBinding.inflate(inflater)
+        return fragmentInterviewBinding.root
+    }
+
+}
