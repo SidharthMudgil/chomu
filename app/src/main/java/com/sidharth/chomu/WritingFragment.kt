b9c4b3919bcb7048eca1c26407b26436b231dcd0
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-11-12 15:50:39 +0530

    add WritingFragment

---
+++ app/src/main/java/com/sidharth/chomu/WritingFragment.kt
@@ -0,0 +1,21 @@
+package com.sidharth.chomu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sidharth.chomu.databinding.FragmentWritingBinding
+
+class WritingFragment : Fragment() {
+
+    private lateinit var fragmentWritingBinding: FragmentWritingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentWritingBinding = FragmentWritingBinding.inflate(inflater)
+        return fragmentWritingBinding.root
+    }
+}
