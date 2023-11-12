b6bbf7de00ae4f04d16ce0825e3fe78d368f394b
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-11-12 15:44:31 +0530

    add HomeFragment

---
+++ app/src/main/java/com/sidharth/chomu/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.sidharth.chomu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sidharth.chomu.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var fragmentHomeBinding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentHomeBinding = FragmentHomeBinding.inflate(inflater)
+        return fragmentHomeBinding.root
+    }
+
+}
