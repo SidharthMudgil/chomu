530f5bbb2bc2111fbc3bf4a47068dcf6122dde47
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-11-12 15:48:16 +0530

    add SocialFragment

---
+++ app/src/main/java/com/sidharth/chomu/SocialFragment.kt
@@ -0,0 +1,22 @@
+package com.sidharth.chomu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sidharth.chomu.databinding.FragmentSocialBinding
+
+class SocialFragment : Fragment() {
+
+    private lateinit var fragmentSocialBinding: FragmentSocialBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentSocialBinding = FragmentSocialBinding.inflate(inflater)
+        return fragmentSocialBinding.root
+    }
+
+}
