e228b0f24430c7ffff3c37ae8ba51b367d24b179
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-11-12 15:47:04 +0530

    add ResultFragment

---
+++ app/src/main/java/com/sidharth/chomu/ResultFragment.kt
@@ -0,0 +1,22 @@
+package com.sidharth.chomu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sidharth.chomu.databinding.FragmentResultBinding
+
+class ResultFragment : Fragment() {
+
+    private lateinit var fragmentResultBinding: FragmentResultBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentResultBinding = FragmentResultBinding.inflate(inflater)
+        return fragmentResultBinding.root
+    }
+
+}
