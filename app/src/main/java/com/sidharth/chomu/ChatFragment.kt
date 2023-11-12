24bbaedce56eba2fd830b30d5c17fc3a4917195e
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-11-12 15:40:33 +0530

    add ChatFragment

---
+++ app/src/main/java/com/sidharth/chomu/ChatFragment.kt
@@ -0,0 +1,21 @@
+package com.sidharth.chomu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sidharth.chomu.databinding.FragmentChatBinding
+
+class ChatFragment : Fragment() {
+    private lateinit var fragmentChatBinding: FragmentChatBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentChatBinding = FragmentChatBinding.inflate(inflater)
+        return fragmentChatBinding.root
+    }
+
+}
