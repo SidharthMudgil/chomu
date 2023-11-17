b06105b0c5578116062d2f923bdbe85e9a16a580
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-11-17 13:43:09 +0530

    add enum ResultStyle

---
+++ app/src/main/java/com/sidharth/chomu/core/enums/ResultStyle.kt
@@ -0,0 +1,12 @@
+package com.sidharth.chomu.core.enums
+
+enum class ResultStyle {
+    NONE,
+    POETIC,
+    PHILOSOPHER,
+    CHEF,
+    EXPLORER,
+    SCIENTIST,
+    COMEDIAN,
+    FUTURIST
+}
